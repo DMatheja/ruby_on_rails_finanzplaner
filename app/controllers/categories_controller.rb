0fb6aa1f8c2d57576590406200c5e6b979bb75a4
Simon-Hauck <SHauck233@gmx.de>
2026-06-09 17:33:33 +0200

    Simon (#10)
    * added chart on dashbord for graphical visualization
    
    * added Sparzielrechner
    
    * added last visited block
    
    * added admin time travel
    
    * quick fix renamed folder
    
    * renamed show.html.rb to show.html.erb to fix error
    
    * fixed create new user form, fixed delete user button, added edit password in edit form
    
    * deleted "actions" column in users table view
    
    * deleted "actions" column from all overview tables except categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,7 @@ class CategoriesController < ApplicationController
       redirect_to @category, notice: 'Category was successfully created.'
     else
       render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -34,6 +35,7 @@ class CategoriesController < ApplicationController
       redirect_to @category, notice: 'Category was successfully updated.'
     else
       render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
